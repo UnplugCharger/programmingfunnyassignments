586758ec4801c442716450be24708b72692fd42b
UnplugCharger <isaacbyron@gmail.com>
2022-03-14 22:31:10 +0300

    wip

--- grep-app/main.go
+++ grep-app/main.go
@@ -8,21 +8,27 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("missing two required positional arguments: fileName  and searchString")
+	}
 	//os.Args provides access to raw command-line arguments. Note that the first
 	//value in this slice is the path to the program, and os.Args[1:] holds the
 	//arguments to the program.
 	args := os.Args
-
-	file, err := os.Open(args[1])
+	fileName := args[1]
+	searchTerm := args[2:]
+	file, err := os.Open(fileName)
 	if err != nil {
-		log.Fatal(err)
+
+		fmt.Printf("failed to open file : %s\n", fileName)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() { // internally, it advances token based on sperator
+	for scanner.Scan() { // internally, it advances token based on separator
 		fmt.Println(scanner.Text()) // token in unicode-char
 
+	}
 	}
 }
